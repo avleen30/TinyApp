57ad659551088327777c2e508584582630681561
Avleen <avleen3@gmail.com>
2017-08-04 00:18:44 +0000

    adding username form and rendering it on pages if user is logged in

--- express_server.js
+++ express_server.js
@@ -2,6 +2,10 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+var cookie = require('cookie');
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 //The body-parser library will allow us to access POST request parameters,
 //such as req.body.longURL, which we will store in a variable called urlDatabase
 
@@ -17,21 +21,23 @@ var urlDatabase = {
 
 //creating route for rendering the list or table of the URLs and their shortened forms
 app.get("/urls", (req, res) => {
-  let templateVars = { urls : urlDatabase };
+  let templateVars = { urls : urlDatabase , username: req.cookies["username"] };
   res.render("urls_index", templateVars);
-})
+});
 
 //We will use the urls_new.ejs template to render the endpoint, /urls/new.
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 //creating route for rendering the full URL and its shortened form
 app.get("/urls/:id", (req, res) => {
 
-  let templateVars = { shortURL : req.params.id, longURL : urlDatabase[req.params.id]};
+  let templateVars = { shortURL : req.params.id, longURL : urlDatabase[req.params.id], username: req.cookies["username"]};
+
   res.render("urls_show", templateVars);
-})
+});
 
 //the urls_new.ejs template that we created uses method="post".
 //This corresponds with the app.post(...) on the server-side code!
@@ -52,6 +58,19 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+//In order to handle the form submission, add an endpoint to handle a POST to /login in your Express server.
+//Use the endpoint to set the cookie parameter called username to the value submitted in the request body via the form
+app.post("/login", (req, res) => {
+  let username = req.body.username
+    if(!username){
+      res.redirect("/urls")
+    }
+    else{
+      res.cookie("username", username); //res.cookie(name, value [, options])
+      res.redirect("/urls")
+    }
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -63,4 +82,4 @@ function generateRandomString() {
    for (var i = 0; i < 6; i++)
       text += possible.charAt(Math.floor(Math.random() * possible.length));
    return text;
-}
+};
